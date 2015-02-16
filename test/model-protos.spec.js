436f98f5279d2213fa60f2497f44e63232676be9
Mark Bevels <mark.bevels@gmail.com>
2015-02-16 20:53:52 +0000

    proto specs

---
+++ test/model-protos.spec.js
@@ -0,0 +1,44 @@
+var Hapi = require('hapi'),
+	Hoek = require('hoek'),
+	Joi = require('joi'),
+	Code = require('code'),
+	expect = Code.expect,
+	Lab = require('lab'),
+	lab = exports.lab = Lab.script(),
+	describe = lab.describe,
+	it = lab.it,
+	before = lab.before,
+	after = lab.after,
+	ModelFactory = require('../lib/model-factory');
+
+describe('model prototype methods', function() {
+	
+	it('should insert and parse', function (done) {
+		done('missing');
+	});
+
+	it('should find and parse', function (done) {
+		done('missing');
+	});
+
+	it('should find one and parse', function (done) {
+		done('missing');
+	});
+
+	it('should find one and parse', function (done) {
+		done('missing');
+	});
+
+	it('should force find (and parse)', function (done) {
+		done('missing');
+	});
+
+	it('should find one and parse', function (done) {
+		done('missing');
+	});
+	
+	it('should validate', function (done) {
+		done('missing');		
+	});
+	
+});
