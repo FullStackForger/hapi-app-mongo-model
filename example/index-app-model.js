233cf9fa5871ff67b645169889864ed002cda874
Mark Bevels <mark.bevels@gmail.com>
2015-02-16 07:25:55 +0000

    model registration method

--- example/index-app-model.js
+++ example/index-app-model.js
@@ -1,6 +1,7 @@
 var AppModel = require('../lib/app-model');
 
 AppModel.connect({
+	
 	url: 'mongodb://localhost:27017/test',
 	opts: {
 		"safe": true,
@@ -8,10 +9,32 @@ AppModel.connect({
 			"native_parser": false
 		}
 	}	
+	
 }).then(function(manager) {
-	manager.db.get('test').insert({
-		name: "monko monko"
+	
+	var News = AppModel.register({
+		collection: 'news',
+		path: __dirname + "/models/news"
 	});
+
+	/* todo: move to tests
+	News.insert({
+		name: "super monko oki donko"
+	}).then(function(result) {
+		console.log(result);
+	});
+	*/
+	
+	/* todo: move to tests
+	News.updateById('54e19a307b945e0000b9ca6f', {
+		$set: { betterVersion: true }
+	}).then(function(result) {
+		console.log(result);
+	})
+	*/
+
+}).catch(function(error) {
+    throw error;
 });
 
 
