a82a45759f656f08c723b5246380d4778879f3a1
Mark Bevels <mark.bevels@gmail.com>
2015-02-16 18:57:48 +0000

    insertAndParse, findAndParse, findOneAndParse

--- example/index-app-model.js
+++ example/index-app-model.js
@@ -72,6 +72,41 @@ AppModel.connect({
 		});
 	 */
 	
+	/* todo: move to tests 
+	var news = [{
+		    title: 'good lorem news',
+		    copy: 'lorem to ipsum lorem to ipsum lorem to ipsum'
+		}, {
+			title: 'o babe'
+		}];
+
+	News.insertAndParse(news)
+		.then(function(news) {
+			// returns array of model objects
+			console.log(news[0].toJSON());
+			console.log(news[1].toJSON());
+		});
+
+	News.insertAndParse(news[0])
+		.then(function(news) {
+			// returns model object
+			console.log(news.toJSON());
+		});
+	 */
+	
+	/* todo: move to tests
+	News.findAndParse({title: 'good lorem news'})
+		.then(function(news) {
+			for (var i = 0; i < news.length; i++) {
+				console.log(news[i].toJSON());					
+			}					
+		});
+	
+	News.findOneAndParse({title: 'good lorem news'})
+		.then(function(news) {
+			console.log(news.toJSON());
+		});
+	*/
 	
 }).catch(function(error) {
     throw error;
