5fe5c7bad24ed72b6ed36a11a2bc6625617d9470
Mark Bevels <mark.bevels@gmail.com>
2015-02-18 04:23:33 +0000

    test coverage on prototypal methods

--- test/model-protos.spec.js
+++ test/model-protos.spec.js
@@ -1,5 +1,6 @@
 var Hapi = require('hapi'),
 	Hoek = require('hoek'),
+	Sinon = require('sinon'),
 	Joi = require('joi'),
 	Code = require('code'),
 	expect = Code.expect,
@@ -7,13 +8,15 @@ var Hapi = require('hapi'),
 	lab = exports.lab = Lab.script(),
 	describe = lab.describe,
 	it = lab.it,
+	after = lab.after,
 	before = lab.before,
 	beforeEach = lab.beforeEach,
 	Model = require('../lib/app-model'),
 	NewsModel;
 
-describe('model prototype methods', function() {
 
+describe('model prototype methods', function() {
+	
 	before(function (done) {
 		Model.connect({ url: 'mongodb://localhost:27017/test', opts: { 'safe': true } })
 			.then(function() {
@@ -36,15 +39,28 @@ describe('model prototype methods', function() {
 		expect(news.copy).to.be.equal(newsData.copy);
 		done();
 	});
-	
-	it('should save', function (done) {
-		var news = new NewsModel({ title: 'new title', copy: 'lorem ipsum' });
-		expect(news).to.be.equal(news);
-		news.title = 'title changed';
-		news.save()
-			.then(function(news) {
-				expect(news.title).to.be.equal('title changed');
-			});
+
+
+	it('should create empty', function (done) {
+		var news = new NewsModel();
+		expect(news).to.not.be.null();
+		done();
+	});
+
+	it('should create ignoring function properties', function (done) {
+		var news = new NewsModel({
+			testMe: function () {}
+		});
+
+		expect(news.testMe).to.be.undefined;
+		done();
+	});
+
+	it('should create ignoring prototype properties', function (done) {
+		var News = function () {};
+		News.prototype.ignored = 'should be ignored';
+
+		expect((new NewsModel(new News())).ignored).to.be.undefined;
 		done();
 	});
 
@@ -61,6 +77,59 @@ describe('model prototype methods', function() {
 		done();
 	});
 
+	it('should validate model with id', function (done) {
+		var news = new NewsModel({_id: '54e3f7722db5ef830aea1fbd', title: 'new title', copy: 'lorem ipsum' }),
+			validated = news.validate();		
+		
+		expect(validated.error).to.be.null;
+		done();
+	});
+
+	it('should save', function (done) {
+		var news = new NewsModel({ title: 'new title', copy: 'lorem ipsum' });
+
+		news.title = 'title changed';
+		news.save()
+			.then(function(news) {
+				expect(news.title).to.be.equal('title changed');
+			});
+		done();
+	});
+
+	it('should reject save for invalid model', function (done) {
+		var news = new NewsModel({ header: 'header is not allowed'});
+		news.save()
+			.then(function() {				
+				done('model with invalid property has been inserted');
+			}).onReject(function(error){
+				expect(error).to.not.be.null;
+				done();
+			});
+	});
+
+	it('should reject save when db error occurs', function (done) {
+		var news = new NewsModel({ title: 'new title', copy: 'lorem ipsum' }),
+			findAndModifyStub;
+
+		findAndModifyStub = Sinon.stub(require('monk').Collection.prototype, "findAndModify", function () {
+			arguments[arguments.length - 1]("Error occurred", null);
+		});
+
+		function complete(msg) {
+			findAndModifyStub.restore();
+			done(msg);
+		}
+
+		news.save()
+			.then(function(news) {
+				complete("model has been marked as inserted in spite of database error");
+			}).onReject(function (error) {
+				expect(error).to.not.be.null;
+				complete();
+			});
+	});
+
+	
 	it('should turn to JSON', function (done) {
 		var newsData = { title: 'new title', copy: 'lorem ipsum' },
 			news = new NewsModel(newsData);
